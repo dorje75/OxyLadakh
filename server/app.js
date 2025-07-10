a63dfcf52da9fee81c7aa74b22c76cd5d02c9a54
dorje75 <dorje.wm@gmail.com>
2025-07-10 14:21:18 +0530

    Updated package.json

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json()); 
 
 app.get('/', (req, res) => {
-  res.send('Oxygen Delivery API is running');
+  res.send('Oxygen Delivery API is running properly');
 });
 
 
